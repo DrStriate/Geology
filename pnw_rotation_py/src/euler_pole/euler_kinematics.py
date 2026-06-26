Fix bug in rotation sense from lat/long to dist translation

pnw_rotation_py/src/euler_pole/euler_kinematics.py
@@ -27,10 +27,10 @@ def getHat(p): # returns a normal to the lat,long point
     np.sin(p['phi'])
     ])
 
-def get_northerly_easterly_from_lat_long_pts(lat1, lon1, lat2, lon2):
+def get_northerly_easterly_from_lat_long_pts(lon1, lat1, lon2, lat2):
     # inv() expects (lon1, lat1, lon2, lat2)
     # forward_azimuth is the angle from point 1 to point 2 (degrees clockwise from North)
-    forward_azimuth, back_azimuth, distance_meters = geod.inv(lon1, lat1, lon2, lat2)
+    forward_azimuth, back_azimuth, distance_meters = geod.inv(lon2, lat2, lon1, lat1)
     
     # Convert azimuth to radians
     azimuth_rad = np.radians(forward_azimuth)

pnw_rotation_py/tests/test_gauss_newton.py
@@ -38,7 +38,7 @@ def test_rotation():
   #print(f"Rotate X:  {x}")
   assert x['r'] == pytest.approx(dtheta, abs=1e-6)
 
-def test_euler_test_quad(): # Need to reconcile gauss_newton rotations w. Euler rotations
+def test_euler_test_quad():
   #test setup
   euler_pole = {"lat" : 45.0,  "long" : -90, "omega" : 1.23 }
   bearings  = [45.0, 135.0, 225.0, 315.0]
@@ -82,7 +82,7 @@ def test_using_north_rotation():
   crop = 0.5 # 50% cropped out
 
   R = 6371.0E3 # Earth radius in m
-  north_v_for_pole_rot= np.sin(-np.radians(test_omega)) * R
+  north_v_for_pole_rot= np.sin(np.radians(test_omega)) * R
 
   sample_n, sample_e, v_east, v_north = \
     tek.create_random_sample_ring(euler_pole, 